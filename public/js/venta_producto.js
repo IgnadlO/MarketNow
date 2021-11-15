09209a75712e840bde8f24300fed5775912a6a3e
IgnadlO <ignadela12@gmail.com>
2021-11-15 11:55:46 -0300

    15/11-Vemder_Productos

--- public/js/venta_producto.js
+++ public/js/venta_producto.js
@@ -1,8 +1,8 @@
 /*----------Variables----------*/
 const cdb = document.getElementById('cdb');
-const cdc = document.getElementById('cdc');
 const cuerpo = document.getElementById('cuerpo');
 const cuerpo2 = document.getElementById('cuerpo2');
+const subtotal = document.getElementById('subtotal');
 let cuerpo3 = document.getElementById('cuerpo3');
 const nom = document.getElementById('Nombre_producto');
 let monto =0;
@@ -16,6 +16,8 @@ document.getElementById("cdb")
 .addEventListener("keyup",pepe);
 document.getElementById('cancelarVenta')
 .addEventListener("click",cancelarcompra);
+document.getElementById('cant')
+.addEventListener("keyup", actSubtotal);
 
 let info;
 fetch('/comercio/productos')
@@ -25,18 +27,51 @@ fetch('/comercio/productos')
       console.log(info)
       for(let i = 0; i < info.length; i++) {
         cuerpo.insertAdjacentHTML('beforeend', `
-        <tr><td>${info[i].nombre}</td><td>${info[i].precioVenta}</td><td>${info[i].cdb}</td></tr>`)
+        <tr onclick="seleccionarProducto(${info[i].cdb}, '${info[i].nombre}', ${info[i].precioVenta})"><td>${info[i].nombre}</td><td>${info[i].precioVenta}</td></tr>`)
         }
     })
     .catch((error) => {
       console.log(error);
     });
+
+let actPrecio = 0;
+
+function seleccionarProducto(cdb, nombre, precio){
+  const codigo = document.getElementById('cdb');
+  nom.value = nombre;
+  subtotal.innerHTML = precio;
+  actPrecio = precio;
+  codigo.value = cdb;
+  document.getElementById('cant').focus();
+}
+
+function presionoTeclaBody(e){
+  if(e.key != 'Enter') return 0;
+  const codigo = document.getElementById('cdb');
+  if(codigo.value != null && codigo.value != undefined && codigo.value != ''){
+    productos();
+    filtrador_nombre();
+  }
+}
+
+function actSubtotal(){
+  if(document.getElementById('cdb').value == '') return 0;
+  const valor = document.getElementById('cant').value;
+  if(valor == '') subtotal.innerText = actPrecio;
+  if(isNaN(valor) || valor == '') return 0;
+  const subOld = parseInt(subtotal.innerText, 10);
+  const nValor = (parseInt(valor, 10) * actPrecio);
+  subtotal.innerText = nValor;
+}
+
+
 /*----------Funciones----------*/
-function filtrador_nombre() {
+function filtrador_nombre(e) {
   var input, filter, table, tr, td, i, j, visible,precio,producto;
   input = document.getElementById('Nombre_producto');
   filter = input.value.toUpperCase();
   tr = cuerpo.getElementsByTagName("tr");
+  const visibles = [];
 
   for (i = 0; i < tr.length; i++) {
     visible = false;
@@ -50,19 +85,26 @@ function filtrador_nombre() {
     if (visible === true) {
       tr[i].style.display = "";
       precio = td[1];
-      
+      if(e != undefined && e.key == 'ArrowRight') {
+        tr[i].onclick();
+        break;
+      }
     } else {
       tr[i].style.display = "none";
     }
   }
-}  
+  console.log(visibles)
+}
 function cancelarcompra(){
   let total = document.getElementById("total");
   let cant = document.getElementById("cant");
   cuerpo2.innerHTML="";
-  total.innerText="Total:$ ";
+  total.innerText = 0;
+  actPrecio = 0;
   monto = 0;
   p2 = [];
+  idsAcumulados = [];
+  location.reload();
 }
 
 function pepe(e){
@@ -70,24 +112,26 @@ function pepe(e){
 }
 
 
-function borrar_product() {
-  var elmtTable = document.getElementById('cuerpo2');
-var tableRows = elmtTable.getElementsByTagName('tr');
-var rowCount = tableRows.length;
-
-/*for (var x=rowCount-0; x>0; x--) {
-   elmtTable.removeChild(tableRows[x]);
-}*/
-rowCount.innerHTML="";
-  console.log("pepe");  
+function borrar_product(product, precio) {
+  let elmtTable = document.getElementById('cuerpo2');
+  let tableRows = document.getElementById('tr' + product);
+  const cantidad = document.getElementById('cant' + product);
+  const oldMonto = parseInt(cantidad.innerText, 10) * precio;
+  const oldTotal = document.getElementById("total").innerText;
+  const nuevoMonto = (parseInt(oldTotal, 10) - oldMonto)
+  document.getElementById("total").innerText = nuevoMonto;
+  monto = nuevoMonto;
+  idsAcumulados.splice(idsAcumulados.indexOf(product), 1);
+  p2.splice(idsAcumulados.indexOf(product), 1);  
+  elmtTable.removeChild(tableRows)
 }
 
 
 function productos(){
-  var total =document.getElementById("total");
+  var total = document.getElementById("total");
   let nuevaCantidad = 1;
   const codigoB = parseInt(cdb.value);
-  let caca;
+  let datosProducto;
   if(cant.value != '' || cant.value != 0) nuevaCantidad = parseInt(cant.value);
   if(idsAcumulados.includes(codigoB)) {
     p2.forEach(val => {
@@ -97,32 +141,42 @@ function productos(){
     }
     })
   } else {
-  caca = info.filter(val=>{
+  datosProducto = info.filter(val=>{
     if (val.cdb == codigoB){
       val.cantidad=nuevaCantidad;
       return true;}
   })
-  p2.push(caca[0]);
-  idsAcumulados.push(caca[0].cdb)
-  monto += parseInt(caca[0].precioVenta) * nuevaCantidad;
+  p2.push(datosProducto[0]);
+  idsAcumulados.push(datosProducto[0].cdb)
+  monto += parseInt(datosProducto[0].precioVenta) * nuevaCantidad;
   }
-  
+
   if(document.getElementById('td' + codigoB) != undefined) {
     const cantidad = document.getElementById('cant' + codigoB);
     const oldCant = parseInt(cantidad.innerText);
     cantidad.innerText = oldCant + nuevaCantidad;
+    const oldReg = document.getElementById('tr' + codigoB)
   } else {
     tr = cuerpo.getElementsByTagName("tr");
-   cuerpo2.insertAdjacentHTML('beforeend', `
-      <tr><td id="td${caca[0].cdb}">${caca[0].cdb}</td><td>${caca[0].nombre}</td><td>$${caca[0].precioVenta}</td><td id="cant${caca[0].cdb}">${nuevaCantidad}</td><td><i class="fas fa-times-circle quitar" onclick="borrar_product()"></i></td></tr>`)
+    const regTabla = `<tr id='tr${datosProducto[0].cdb}'>
+      <td id="td${datosProducto[0].cdb}">${datosProducto[0].cdb}</td>
+      <td>${datosProducto[0].nombre}</td>
+      <td>$${datosProducto[0].precioVenta}</td>
+      <td id="cant${datosProducto[0].cdb}">${nuevaCantidad}</td>
+      <td><i class="fas fa-times-circle quitar" onclick="borrar_product(${datosProducto[0].cdb}, ${datosProducto[0].precioVenta})"></i></td>
+      </tr>`;
+   cuerpo2.insertAdjacentHTML('beforeend', regTabla)
   }
-  total.innerText = "Total:$ "+ monto;
+  total.innerText = monto;
   cdb.value = '';
+  subtotal.innerHTML = 0;
+  document.getElementById('Nombre_producto').value = '';
+  document.getElementById('cant').value = '';
+  actPrecio = 0;
 }
 
 function subirVenta() {
   const ventas = p2;
-  const idCliente = cdc.value;
   console.log(ventas)
   fetch("http://localhost:3000/comercio/venderProducto", {
     method: "POST",
